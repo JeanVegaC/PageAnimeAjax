4f67ad96fd6b2a1f9abf16d92afaa4355e0c76ab
Jhean Vega <jhonivegacasimiro@gmail.com>
2022-08-19 10:55:33 -0500

    fix routes

--- routes/animes.js
+++ routes/animes.js
@@ -12,7 +12,7 @@ router.get('/', (req,res)=>{
     res.send(animes);
 })
 
-router.get('/:id', async(req,res)=>{  
+router.get('/id/:id', async(req,res)=>{  
     
 function getAnimeId (id){
     return(
